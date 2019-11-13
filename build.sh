73366db4378cf5190000990566dab54964289d7a
J-Donald Tournier <jdtournier@gmail.com>
2019-11-13 13:15:20 +0000

    Add Eigen include path to recipe

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 unset LD ARCH
 export CFLAGS="-idirafter $CONDA_PREFIX/include" 
 export LDFLAGS="-L$CONDA_PREFIX/lib"
+export EIGEN_CFLAGS="-idirafter $CONDA_PREFIX/include/eigen3"
 mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
 mkdir -p "$PREFIX/share"
