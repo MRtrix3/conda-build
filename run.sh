479b73781e748ff65e442cb7b39cd0b29aa89408
J-Donald Tournier <jdtournier@gmail.com>
2019-11-29 20:37:16 +0000

    fix up behaviour on Linux

--- run.sh
+++ run.sh
@@ -8,8 +8,7 @@ else
 
   sed -i'.orig' -e " s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" meta.yaml
 
-  [[ "x$4" != "x" ]] && CONDA="$4/bin/"
-  ${CONDA}conda-build .
+  "$4"/bin/conda-build .
 
   pkgfile=$(${CONDA}conda-build . --output)
   mv $pkgfile $3-$(basename $pkgfile)
