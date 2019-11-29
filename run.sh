d63fedaedcb716a16a6c56040eafee86eb27ba80
J-Donald Tournier <jdtournier@gmail.com>
2019-11-29 20:41:37 +0000

    remove setting CONDA_ROOT

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $# -lt 3 ]]; then
-  echo usage: run.sh tag user prefix [CONDA_ROOT]
+if [[ $# -ne 3 ]]; then
+  echo usage: run.sh tag user prefix
 else
 
   set -ex
 
   sed -i'.orig' -e " s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" meta.yaml
 
-  "$4"/bin/conda-build .
+  "$CONDA"/bin/conda-build .
 
-  pkgfile=$(${CONDA}conda-build . --output)
+  pkgfile=$("$CONDA"/bin/conda-build . --output)
   mv $pkgfile $3-$(basename $pkgfile)
 
 fi
