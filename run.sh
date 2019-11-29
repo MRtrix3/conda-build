4774b4c2cba0dda5fb8b2b5136907f78741273bc
J-Donald Tournier <jdtournier@gmail.com>
2019-11-29 20:26:12 +0000

    allow passing CONDA_ROOT via command-line

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 3 ]]; then
-  echo usage: run.sh tag user prefix
+if [[ $# -lt 3 ]]; then
+  echo usage: run.sh tag user prefix [CONDA_ROOT]
 else
 
   set -ex
 
   sed -i'.orig' -e " s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" meta.yaml
 
-  [[ "x$CONDA" == "x" ]] || CONDA="$CONDA/bin/"
+  [[ "x$4" != "x" ]] && CONDA="$4/bin/"
   ${CONDA}conda-build .
 
   pkgfile=$(${CONDA}conda-build . --output)
