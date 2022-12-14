eec548949a89affe76b08325123098c5b3342d3a
J-Donald Tournier <jdtournier@gmail.com>
2022-12-14 20:36:34 +0000

    Remove rpath from link flags to allow building Linux package

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 unset LD LINK ARCH
 export CFLAGS="-idirafter $CONDA_PREFIX/include" 
 export LDFLAGS="-L$CONDA_PREFIX/lib"
-export LINKFLAGS="-L$CONDA_PREFIX/lib -Wl,-rpath,${CONDA_PREFIX}/lib"
+export LINKFLAGS="-L$CONDA_PREFIX/lib"
 export EIGEN_CFLAGS="-idirafter $CONDA_PREFIX/include/eigen3"
 mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
