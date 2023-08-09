6e7b925573dd4a70ee6feea9aaa230d013afc812
J-Donald Tournier <jdtournier@gmail.com>
2023-08-09 17:53:12 +0100

    update recipe to match changes in conda

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ else
 
   sed -i'.orig' -e " s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" meta.yaml
 
-  "$CONDA"/bin/conda-build .
+  "$CONDA"/bin/conda build .
 
-  pkgfile=$("$CONDA"/bin/conda-build . --output)
+  pkgfile=$("$CONDA"/bin/conda build . --output)
   mv $pkgfile $3-$(basename $pkgfile)
 
 fi
